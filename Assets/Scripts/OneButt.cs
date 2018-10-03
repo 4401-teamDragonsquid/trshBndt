2f479a4afeffc01fd620f95c812c62d568aad971
Ren <la235215@ohio.edu>
2018-10-03 15:57:28 -0400

    trshbndt first file

---
+++ Assets/Scripts/OneButt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneButt : MonoBehaviour 
+{
+
+	public void ButtonAction()
+	{
+		if (gameObject.activeInHierarchy == false) {
+			gameObject.SetActive (true);
+		} else
+			gameObject.SetActive (false);
+	
+	}
+}
---
+++ Assets/Scripts/RoadPuzzle.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class RoadPuzzle: MonoBehaviour {
+
+
+	public bool one;
+	public bool two;
+	public bool three;
+	public bool four;
+	public bool five;
+	public bool six;
+	public bool seven;
+	public bool eight;
+	public bool nine;
+	public bool a;
+	public bool b;
+	public bool c;
+	public bool d;
+	public bool e;
+	public bool f;
+	public bool g;
+	public bool h;
+	public bool i;
+	public bool j;
+	public bool k;
+	public bool l;
+
+
+	// Use this for initialization
+	void Start () {
+
+		one =false;
+		two = false;
+		three = false;
+		four = false;
+		five = false;
+		six = false;
+		seven = false;
+		eight = false;
+		nine = false;
+
+		a = true;
+		b = true;
+		c = true;
+		d = true;
+		e = true;
+		f = true;
+		g = true;
+		h = true;
+		i = true;
+		j = true;
+		k = true;
+		l = true;
+
+		Debug.Log (one);
+		Debug.Log (a);
+
+		// Update is called once per frame
+		//void Update () 
+	//	{
+		//	if (Toggle);
+	//  }
+		}
+	}
