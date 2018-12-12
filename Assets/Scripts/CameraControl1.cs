b97501d78e19b117cbe0ebd9ef9b71700b285a14
Dylan A Wright <dw933214@ohio.edu>
2018-12-12 04:00:56 -0500

    Fixed Mesh, Added more camera control

---
+++ Assets/Scripts/CameraControl1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl1 : MonoBehaviour {
+    public GameObject cameraPosition1;
+    public GameObject cameraPosition2;
+    public GameObject theCamera;
+    // Update is called once per frame
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision");
+        if (theCamera.transform.position == cameraPosition2.transform.position)
+        
+            {
+            theCamera.transform.position = cameraPosition1.transform.position;
+            Debug.Log("If Statement");
+            }
+       //if (theCamera.transform.position == cameraPosition2.transform.position)
+    }
+}
